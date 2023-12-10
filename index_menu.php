040f6299328fcfa11cbe95b2ceed31a605938cd0
Nur Auni <141117130+43003@users.noreply.github.com>
2023-12-10 14:20:04 +0800

    updatae logo at head and menu

--- index_menu.php
+++ index_menu.php
@@ -18,7 +18,7 @@
             <div class="sidenav-header">
                 <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
                 <a class="navbar-brand m-0" href="index.php">
-                    <img src="assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+                    <img src="assets/img/team.png" class="navbar-brand-img h-100" alt="main_logo">
                     <span class="ms-1 font-weight-bold text-white">TeamHub</span>
                 </a>
             </div>
